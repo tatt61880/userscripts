23235e0db0f6a5f6e8edcf02c30bd34648836c1c
tatt61880 <tatt61880@gmail.com>
2020-01-04 21:23:45 +0900

    prettier

--- atcoder-language-check.user.js
+++ atcoder-language-check.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        atcoder-language-check
 // @namespace   https://github.com/tatt61880
-// @version     1.0.0
+// @version     1.2.0
 // @description AtCoderでの提出時の言語チェック用です。
 // @author      tatt61880
 // @match       https://atcoder.jp/contests/*
@@ -11,41 +11,49 @@
 // ==/UserScript==
 
 (function($) {
-    'use strict';
-    const myLang = 'C++14 (GCC 5.4.1)';
+  'use strict';
+  const myLang = 'C++14 (GCC 5.4.1)';
 
-    function getElement() {
-        if (location.href.match('/submit')) {
-            $('#select-lang > div').each(function(index, elem){
-                if ($(elem).css('display') == 'block') {
-                    return $(elem).find('span.selection');
-                }
-            })
-        } else {
-            return $('#select-lang span.selection');
+  function getElement() {
+    if (location.href.match('/submit')) {
+      $('#select-lang > div').each(function(index, elem) {
+        if ($(elem).css('display') == 'block') {
+          return $(elem).find('span.selection');
         }
+      });
+    } else {
+      return $('#select-lang span.selection');
     }
+  }
 
-    function checkLanguage() {
-        const lang = getElement().text();
-        const isOk = (lang.indexOf(myLang) !== -1);
-        $('#select-lang').css('background', isOk ? '#FFFFFF' : '#FF0000');
+  function checkLanguage() {
+    const lang = getElement().text();
+    const isOk = lang.indexOf(myLang) !== -1;
+    $('#select-lang').css('background', isOk ? '#FFFFFF' : '#FF0000');
+  }
+
+  addEventListener('load', checkLanguage);
+
+  let timeoutId;
+  addEventListener(
+    'mouseup',
+    function() {
+      // イベントが連続して発火したときに、最後の1回の後だけ処理するようにします。
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(checkLanguage, 500);
+    },
+    {
+      passive: true,
     }
-
-    addEventListener('load', checkLanguage);
-
-    let timeoutId;
-    addEventListener('mouseup', function(){
-        // イベントが連続して発火したときに、最後の1回の後だけ処理するようにします。
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(checkLanguage, 500);
-    }, {
-        passive : true
-    });
-    addEventListener('keydown', function(){
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(checkLanguage, 500);
-    }, {
-        passive : true
-    });
+  );
+  addEventListener(
+    'keydown',
+    function() {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(checkLanguage, 500);
+    },
+    {
+      passive: true,
+    }
+  );
 })(jQuery);
--- atcoder-standings.user.js
+++ atcoder-standings.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        atcoder-standings
 // @namespace   https://github.com/tatt61880
-// @version     1.1.5
+// @version     1.2.0
 // @description AtCoderの順位表のAC時刻に棒グラフ表示を追加します。
 // @author      tatt61880
 // @match       https://atcoder.jp/*/standings
@@ -11,26 +11,36 @@
 // ==/UserScript==
 
 (function() {
-    'use strict';
-    function drawACTimeBarGraph(){
-        const contestTimeSec = (endTime - startTime) / 1000;
-        // console.log('contestTimeSec: ' + contestTimeSec);
-        [].forEach.call(document.getElementsByClassName("standings-result"),function(x){
-            const elem = x.children[1];
-            if (elem === undefined) return;
-            const timeStr = elem.textContent;
-            const timeSec = Number(timeStr.substr(0, timeStr.length - 3)) * 60 + Number(timeStr.substr(-2));
-            const parcent = Math.round(100 * timeSec / contestTimeSec);
-            const style = 'background: linear-gradient(to right, rgb(250, 250, 150) ' + parcent + '%, transparent ' + parcent + '%);';
-            elem.setAttribute('style', style);
-        });
-    }
+  'use strict';
+  function drawACTimeBarGraph() {
+    const contestTimeSec = (endTime - startTime) / 1000;
+    // console.log('contestTimeSec: ' + contestTimeSec);
+    [].forEach.call(
+      document.getElementsByClassName('standings-result'),
+      function(x) {
+        const elem = x.children[1];
+        if (elem === undefined) return;
+        const timeStr = elem.textContent;
+        const timeSec =
+          Number(timeStr.substr(0, timeStr.length - 3)) * 60 +
+          Number(timeStr.substr(-2));
+        const parcent = Math.round(100 * timeSec / contestTimeSec);
+        const style =
+          'background: linear-gradient(to right, rgb(250, 250, 150) ' +
+          parcent +
+          '%, transparent ' +
+          parcent +
+          '%);';
+        elem.setAttribute('style', style);
+      }
+    );
+  }
 
-    addEventListener('load', drawACTimeBarGraph);
+  addEventListener('load', drawACTimeBarGraph);
 
-    let timeoutId;
-    addEventListener('mouseup', function(){
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(drawACTimeBarGraph, 500);
-    });
+  let timeoutId;
+  addEventListener('mouseup', function() {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(drawACTimeBarGraph, 500);
+  });
 })();
