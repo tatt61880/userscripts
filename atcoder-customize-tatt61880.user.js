709c773b8a7490d80f30cdd264db3d5fe61cd03c
tatt61880 <tatt61880@gmail.com>
2020-01-04 19:49:04 +0900

    Update atcoder-customize-tatt61880.user.js

--- atcoder-customize-tatt61880.user.js
+++ atcoder-customize-tatt61880.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        atcoder-customize-tatt61880
 // @namespace   https://github.com/tatt61880
-// @version     1.1.2
+// @version     1.1.3
 // @description AtCoderのサイトをtatt61880の好みに合わせて細かく調整します。
 // @author      tatt61880
 // @match       https://atcoder.jp/*
@@ -17,8 +17,16 @@
     const kRemoveBr = true; // ソースコード提出用テキストエリア下部の《※ 512 KiB まで》《※ ソースコードは「Main.拡張子」で保存されます》の間の改行を削除。
     const kStandingsDefault100 = false; // 1 ページあたり表示数のデフォルトを100に。
     const kInOutColorize = true; // 入出力の文字に色を付ける。
+    // const kShowStandingsStatisticsInThead = true; // 正解者数 / 提出者数 を上部に表示
+    const kShowStandingsStatisticsInThead = true; // ※ソートすると崩れることが判ったので封印。
 
-    if (kFoldingFooter) {
+    if (kFoldingFooter) foldingFooter();
+    if (kRemoveBr) removeBr();
+    if (kStandingsDefault100) standingsDefault100();
+    if (kInOutColorize) inOutColorize();
+    if (kShowStandingsStatisticsInThead) showStandingsStatisticsInThead();
+
+    function foldingFooter() {
         const footerId = 'footer-tatt61880';
         const footer = $('footer.footer').parent();
         footer.after('<div style="text-align: center;">'
@@ -43,7 +51,7 @@
         });
     }
 
-    if (kRemoveBr) {
+    function removeBr() {
         const br = $('#sourceCode').children('p').children('br');
         br.each(function(index, element) {
             const prevText = $(element).prev().text();
@@ -55,41 +63,37 @@
         });
     }
 
-    if (kStandingsDefault100) {
-        if (location.href.match(/^https:\/\/atcoder\.jp\/contests\/.*\/standings/)) {
-            $('.standings-per-page').each(function(index, element) {
-                if ($(element).text() == '100') {
-                    element.click();
-                }
-            });
-        }
+    function standingsDefault100() {
+        if (!location.href.match(/^https:\/\/atcoder\.jp\/contests\/.*\/standings/)) return;
+
+        $('.standings-per-page').each(function(index, element) {
+            if ($(element).text() == '100') {
+                element.click();
+            }
+        });
     }
 
-    if (kInOutColorize) {
-        if (location.href.match(/^https:\/\/atcoder\.jp\/contests\/.*\/tasks.*$/)) {
-            $('h3').each(function(index, element) {
-                if ($(element).text().match(/^入力.*$/)) {
-                    $(element).css('color', '#000080');
-                } else if ($(element).text().match(/^出力.*$/)) {
-                    $(element).css('color', '#008000');
-                }
-            });
-        }
+    function inOutColorize() {
+        if (!location.href.match(/^https:\/\/atcoder\.jp\/contests\/.*\/tasks.*$/)) return;
+
+        $('h3').each(function(index, element) {
+            if ($(element).text().match(/^入力.*$/)) {
+                $(element).css('color', '#000080');
+            } else if ($(element).text().match(/^出力.*$/)) {
+                $(element).css('color', '#008000');
+            }
+        });
     }
 
-    // ソートすると崩れるのを解決するまで封印。
-    // 正解者数 / 提出者数 を上部に表示
-    if (true) {
-        if (location.href.match(/^https:\/\/atcoder\.jp\/contests\/.*\/standings/)) {
-            const elem = $('#standings-tbody').children().eq(-1);
-            $('table > thead').children().eq(0).before(elem);
-            const elem2 = $('#standings-tbody').children().eq(-1);
-            $('table > thead').children().eq(0).before(elem2);
-        }
+    function showStandingsStatisticsInThead() {
+        if (!location.href.match(/^https:\/\/atcoder\.jp\/contests\/.*\/standings/)) return;
+
+        const elem = $('#standings-tbody').children().eq(-1);
+        $('table > thead').children().eq(0).before('<tr>' + elem.html() + '</tr>');
     }
 
-    // 未完成
-    // 入出力の表示位置
+    // 未完成 (完成しなさそう)
+    // 入出力の表示変更
     if (false) {
         if (location.href.match(/^https:\/\/atcoder\.jp\/contests\/.*\/tasks.*$/)) {
             $('h3').each(function(index, element) {
