17bfaf50233e25e7db52ba5a0f9963371b20c630
tatt61880 <tatt61880@gmail.com>
2020-01-04 21:39:27 +0900

    Update atcoder-language-check.user.js

--- atcoder-language-check.user.js
+++ atcoder-language-check.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        atcoder-language-check
 // @namespace   https://github.com/tatt61880
-// @version     1.2.1
+// @version     1.2.2
 // @description AtCoderでの提出時の言語チェック用です。
 // @author      tatt61880
 // @match       https://atcoder.jp/contests/*
@@ -16,11 +16,14 @@
 
     function getElement() {
         if (location.href.match('/submit')) {
+            let res;
             $('#select-lang > div').each(function(index, elem) {
                 if ($(elem).css('display') == 'block') {
-                    return $(elem).find('span.selection');
+                    res = $(elem).find('span.selection');
+                    return false;
                 }
             });
+            return res;
         } else {
             return $('#select-lang span.selection');
         }
