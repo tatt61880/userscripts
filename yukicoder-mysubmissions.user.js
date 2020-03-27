1d7e25228bd3ca3b63f86ba284b4c851b638832f
tatt61880 <tatt61880@gmail.com>
2020-03-27 23:34:21 +0900

    Create yukicoder-mysubmissions.user.js

---
+++ yukicoder-mysubmissions.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        yukicoder-mysubmissions
+// @namespace   https://github.com/tatt61880
+// @version     1.0.0
+// @description yukicoderの「提出一覧」の横に「自分の提出一覧」を追加します。
+// @author      tatt61880
+// @match       https://yukicoder.me/problems/no/*
+// @grant       none
+// @updateURL   https://github.com/tatt61880/userscripts/raw/master/yukicoder-mysubmissions.users.js
+// @downloadURL https://github.com/tatt61880/userscripts/raw/master/yukicoder-mysubmissions.users.js
+// ==/UserScript==
+
+(function($) {
+  'use strict';
+
+  if (!location.href.match(/^https:\/\/yukicoder\.me\/problems\/no\/.*$/)) return;
+  $('a').each(function(index, element) {
+    const text = $(element).text();
+    if (text == '提出一覧') {
+      const url = $(element).attr('href');
+      if (url.match(/^\/problems\/no\/(\d+)\/submissions$/)){
+        const id = url.replace(/^\/problems\/no\/(\d+)\/submissions$/, '$1');
+        $(element).after('<a href="https://yukicoder.me/problems/no/' + id + '/submissions?my_submission=enabled">自分の提出一覧</a>');
+      }
+    }
+  });
+})(jQuery);
