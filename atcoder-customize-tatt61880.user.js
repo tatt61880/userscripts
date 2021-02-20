d2f9a8fa35fcb59347f3ca8d59e1bc956ef31f17
tatt61880 <tatt61880@gmail.com>
2021-02-20 23:52:26 +0900

    Update atcoder-customize-tatt61880.user.js

--- atcoder-customize-tatt61880.user.js
+++ atcoder-customize-tatt61880.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        atcoder-customize-tatt61880
 // @namespace   https://github.com/tatt61880
-// @version     1.5.5
+// @version     1.5.6
 // @description AtCoderのサイトをtatt61880の好みに合わせて細かく調整します。
 // @author      tatt61880
 // @match       https://atcoder.jp/*
@@ -173,7 +173,7 @@
     if (!kLanguageCheck) return;
     if (!location.href.match(/\/contests\//)) return;
 
-    const myLang = 'C++14 (GCC 5.4.1)';
+    const myLang = 'C++ (GCC 9.2.1)';
 
     function getElement() {
       if (location.href.match('/submit')) {
