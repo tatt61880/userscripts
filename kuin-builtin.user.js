4b8a6a604eb3a0527383fcfffd756379aff0aff7
tatt61880 <tatt61880@gmail.com>
2021-09-12 17:01:04 +0900

    .

--- kuin-builtin.user.js
+++ kuin-builtin.user.js
@@ -22,8 +22,8 @@
   {
     // 順番を固定します。
     const types = [nameTypeAll, 'int', 'float', 'char', 'bool',
-                   'bit8', 'bit16', 'bit32', 'bit64',
-                   'list', 'stack', 'queue', 'dict', 'enum'];
+      'bit8', 'bit16', 'bit32', 'bit64',
+      'list', 'stack', 'queue', 'dict', 'enum'];
     for (const type of types) {
       typeSet.add(type);
       typeIds[type] = typeIdNext++;
@@ -34,14 +34,13 @@
     const $div = $('<div>');
     let $elem = $h2s.eq(i);
     let typeInfo = false;
-    while (true) {
+    while ($elem.next()[0] !== undefined) {
       $div.append($elem.clone());
       const $prev = $elem;
       $elem = $elem.next();
       $prev.remove();
       if ($elem[0].nodeName == 'H2') break;
       if ($elem[0].className == 'navi_sub_bottom') break;
-      if ($elem.next()[0] === undefined) break;
       if (!typeInfo && $elem[0].nodeName == 'DIV') {
         const match = $elem.text().match(/対象の型: ((?:[^,]+(?:, )?)*)/);
         if (match) {
@@ -72,18 +71,18 @@
     $('h2').eq(0).parent().before($div);
   }
 
-   $('input[name="type"]').change(function () {
-     const selectedType = $(this).val();
-     const selectedTypeId = typeIds[selectedType];
-     if (selectedType == nameTypeAll) {
-       for (let type of typeSet) {
-         $(`.type${typeIds[type]}`).show();
-       }
-     } else {
-       for (let type of typeSet) {
-         $(`.type${typeIds[type]}`).hide();
-       }
-       $(`.type${selectedTypeId}`).show();
-     }
-   });
+  $('input[name="type"]').change(function () {
+    const selectedType = $(this).val();
+    const selectedTypeId = typeIds[selectedType];
+    if (selectedType == nameTypeAll) {
+      for (let type of typeSet) {
+        $(`.type${typeIds[type]}`).show();
+      }
+    } else {
+      for (let type of typeSet) {
+        $(`.type${typeIds[type]}`).hide();
+      }
+      $(`.type${selectedTypeId}`).show();
+    }
+  });
 })(window.jQuery);
